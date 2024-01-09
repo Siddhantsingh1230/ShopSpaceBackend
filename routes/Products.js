dab33c61e083ea2a9a0278b7cb007520992fa66a
siddhantsingh1230 <siddhantsingh9023134084@gmail.com>
2024-01-09 15:55:57 +0530

    added support for filter response

--- routes/Products.js
+++ routes/Products.js
@@ -11,7 +11,7 @@ import { isAdmin } from "../middlewares/auth.js";
 const router = express.Router();
 
 router.get("/", getAllProducts);
-router.get("/:page/:quantum", getQuantizedProducts);
+router.get("/:page/:quantum/", getQuantizedProducts);
 router.get("/filter", filterProduct);
 router.post("/add", isAdmin, addProduct);
 router.delete("/delete/:id", isAdmin, deleteProduct);
