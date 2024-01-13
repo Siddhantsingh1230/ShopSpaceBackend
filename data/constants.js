976a6d009d773b559f530d075b7a91b3f5d4f8e4
siddhantsingh1230 <siddhantsingh9023134084@gmail.com>
2024-01-13 20:32:18 +0530

    added firebase utils

--- data/constants.js
+++ data/constants.js
@@ -1,4 +1,4 @@
-export const DefaultProfileImageURL = "https://img.freepik.com/free-photo/3d-illustration-cute-clown-with-curly-hair_1142-32471.jpg?t=st=1704254557~exp=1704258157~hmac=be3b938a6e92e2c92139dcfbe2ebd6a8fbdf4109097fa161a39e0e3c5c29ec69&w=740"
+export const DefaultProfileImageURL = "https://img.freepik.com/free-photo/3d-illustration-cute-clown-with-curly-hair_1142-32471.jpg?t=st=1704254557~exp=1704258157~hmac=be3b938a6e92e2c92139dcfbe2ebd6a8fbdf4109097fa161a39e0e3c5c29ec69&w=740";
 
 export const FIREBASE_BASE_FOLDER = "uploads";
 export const FIREBASE_OFFERS_FOLDER = `${FIREBASE_BASE_FOLDER}/offers`;
