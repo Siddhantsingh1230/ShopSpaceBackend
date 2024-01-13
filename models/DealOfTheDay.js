f5ceac7f366c4e21604d673bb8a6bc874c84b7e7
siddhantsingh1230 <siddhantsingh9023134084@gmail.com>
2024-01-13 15:38:58 +0530

    added dealOfTheDay model

---
+++ models/DealOfTheDay.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const dealOfTheDaySchema = new mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "products",
+    required: true,
+  },
+  offerDuration: {
+    type: Date,
+    required:true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+export const dealOfTheDayModel = mongoose.model("dealOfTheDay", dealOfTheDaySchema);
